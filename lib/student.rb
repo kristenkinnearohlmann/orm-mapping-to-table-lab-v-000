b59b780446213c4f873f061f85f9876efa6e2a76
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-04 22:03:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,27 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+      sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade TEXT
+        )
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+
+  end
+
 end
